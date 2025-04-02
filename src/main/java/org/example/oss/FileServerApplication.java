8472284457a8b6ee551142da025eab59374c6676
pineclone <pineclone@outlook.com>
2025-04-03 01:50:46 +0800

    添加nacos注册功能

--- src/main/java/org/example/oss/FileServerApplication.java
+++ src/main/java/org/example/oss/FileServerApplication.java
@@ -3,19 +3,18 @@ package org.example.oss;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FileServerApplication {
 
     public static void main(String[] args) {
         // 加载 .env 到系统属性
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
         dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
-
         SpringApplication.run(FileServerApplication.class, args);
     }
-
 }
